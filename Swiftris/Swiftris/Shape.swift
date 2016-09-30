4975503da312ae58fdd38d19754464e630fa687d
Kenneth Chou <kennethchou83@yahoo.com>
2016-09-30 12:27:45 -0700

    completed play by rules

--- Swiftris/Swiftris/Shape.swift
+++ Swiftris/Swiftris/Shape.swift
@@ -125,8 +125,33 @@ class Shape: Hashable, CustomStringConvertible {
         }
     }
     
+    // #3
+    final func rotateClockwise() {
+        let newOrientation = Orientation.rotate(orientation, clockwise: true)
+        rotateBlocks(newOrientation)
+        orientation = newOrientation
+    }
+    
+    final func rotateCounterClockwise() {
+        let newOrientation = Orientation.rotate(orientation, clockwise: false)
+        rotateBlocks(newOrientation)
+        orientation = newOrientation
+    }
+    
     final func lowerShapeByOneRow() {
         shiftBy(0, rows:1)
+    }
+    
+    final func raiseShapeByOneRow() {
+        shiftBy(0, rows:-1)
+    }
+    
+    final func shiftRightByOneColumn() {
+        shiftBy(1, rows:0)
+    }
+    
+    final func shiftLeftByOneColumn() {
+        shiftBy(-1, rows:0)
     }
     
     // #2
